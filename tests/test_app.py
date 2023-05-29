d7c9bbe58007c6541336dc8528a06d453abc7387
connor-sp <connorferster@gmail.com>
2023-05-28 22:37:47 -0700

    Update tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,27 +1,27 @@
 import pathlib
 import json
-from section_browser import app
+from section_browser import main
 
 TEST_DATA_STORE_FILE = pathlib.Path(__file__).parents[0] / "TEST_DATA_STORE.json"
 
 
 def test_parse_kwargs():
-    assert app._parse_kwargs(["1", "2", "3", "4"]) == {"1": "2", "3": "4"}
-    assert app._parse_kwargs(["--d", "<=304", "--Ix", ">503"]) == {
+    assert main._parse_kwargs(["1", "2", "3", "4"]) == {"1": "2", "3": "4"}
+    assert main._parse_kwargs(["--d", "<=304", "--Ix", ">503"]) == {
         "d": "<=304",
         "Ix": ">503",
     }
 
 
 def test_clear_data_store():
-    app._clear_data_store(TEST_DATA_STORE_FILE)
+    main._clear_data_store(TEST_DATA_STORE_FILE)
     with open(TEST_DATA_STORE_FILE, "r") as test_file:
         test_data = json.load(test_file)
     assert test_data == {"indexes": [], "filters": {}, "loads": {}}
 
 
 def test_set_current_indexes():
-    app._set_current_indexes(
+    main._set_current_indexes(
         [1, 2, 3], {"B": "cat"}, {"C": "scarf"}, TEST_DATA_STORE_FILE
     )
     with open(TEST_DATA_STORE_FILE, "r") as test_file:
@@ -34,7 +34,7 @@ def test_set_current_indexes():
 
 
 def test_get_current_indexes():
-    test_data = app._get_current_indexes(TEST_DATA_STORE_FILE)
+    test_data = main._get_current_indexes(TEST_DATA_STORE_FILE)
     assert test_data == ([1, 2, 3], {"B": "cat"}, {"C": "scarf"})
 
 
@@ -43,10 +43,10 @@ def test_parse_slice():
     slice1 = "0:1"
     slice2 = "0:5:2"
     slice3 = "-1:4:-1"
-    assert app._parse_slice(slice0) == slice(0, 0, None)
-    assert app._parse_slice(slice1) == slice(0, 1, None)
-    assert app._parse_slice(slice2) == slice(0, 5, 2)
-    assert app._parse_slice(slice3) == slice(-1, 4, -1)
+    assert main._parse_slice(slice0) == slice(0, 0, None)
+    assert main._parse_slice(slice1) == slice(0, 1, None)
+    assert main._parse_slice(slice2) == slice(0, 5, 2)
+    assert main._parse_slice(slice3) == slice(-1, 4, -1)
 
 
 def test_parse_comparison_value():
@@ -54,7 +54,7 @@ def test_parse_comparison_value():
     cv1 = ">93.0"
     cv2 = "==34.5"
     cv3 = "!=43.5e6"
-    assert app._parse_comparison_value(cv0) == ("<=", 234.0)
-    assert app._parse_comparison_value(cv1) == (">", 93.0)
-    assert app._parse_comparison_value(cv2) == ("==", 34.5)
-    assert app._parse_comparison_value(cv3) == ("!=", 43.5e6)
+    assert main._parse_comparison_value(cv0) == ("<=", 234.0)
+    assert main._parse_comparison_value(cv1) == (">", 93.0)
+    assert main._parse_comparison_value(cv2) == ("==", 34.5)
+    assert main._parse_comparison_value(cv3) == ("!=", 43.5e6)
